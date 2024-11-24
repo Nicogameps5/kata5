25470045537bd08f0fad67d19720de38ffcd828e
Nico <jkjkloljbjk@gmail.com>
2024-11-24 13:56:44 +0000

    RandomUserGetResponse record

---
+++ src/main/java/software/ulpgc/kata5/RandomUserGetResponse.java
@@ -0,0 +1,21 @@
+package software.ulpgc.kata5;
+
+import java.util.List;
+
+public record RandomUserGetResponse(List<User> results, Info info) {
+    public record User(Name name, String gender, String email, Login login){
+
+    }
+
+    public record Name(String title, String first, String last){
+
+    }
+
+    public record Login(String uuid, String username, String password, String salt){
+
+    }
+
+    public record Info(String seed, int results, int page){
+
+    }
+}
